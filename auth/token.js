75c16887e2088ad3a43789049fba6e53b9ae9124
DmytroVoitovych <begunec23@gmail.com>
2023-03-02 18:09:43 +0200

    fix add extra keys

--- auth/token.js
+++ auth/token.js
@@ -2,9 +2,13 @@ const jwt = require("jsonwebtoken");
 const { SECRET_KEY, REFRESH_KEY } = process.env;
 
 const funcGetToken = (user) => ({
-  token: jwt.sign({ id: user._id }, SECRET_KEY, {
-    expiresIn: "1h",
-  }),
+  token: jwt.sign(
+    { id: user._id, name: user.name, email: user.email },
+    SECRET_KEY,
+    {
+      expiresIn: "1h",
+    }
+  ),
   refresh: jwt.sign({ id: user._id }, REFRESH_KEY, {
     expiresIn: "30d",
   }),
