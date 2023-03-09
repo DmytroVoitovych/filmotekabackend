f02c3535b35cfabe0f00ecd61a85391ea718f4a8
DmytroVoitovych <begunec23@gmail.com>
2023-03-09 10:14:36 +0200

    fix refresh signature

--- utils/funcCheckRefreshToken.js
+++ utils/funcCheckRefreshToken.js
@@ -13,9 +13,9 @@ const funcCheckRefreshToken = async (req, _, next) => {
       const userIP = await User.findOne({ ip });
       // для синхронізації
       if (userIP) {
-        const { token } = userIP;
-        if (token) {
-          jwt.verify(token, REFRESH_KEY);
+        const { refresh } = userIP;
+        if (refresh) {
+          jwt.verify(refresh, REFRESH_KEY);
           req.user = userIP;
           next();
           return;
